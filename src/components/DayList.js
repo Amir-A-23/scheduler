362a7b7ea89460ba8fa654e022b4c2694bd8e96c
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-09 17:07:39 -0600

    Add DayList container to nav bar

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,17 @@ import React from 'react';
 import DayListItem from 'components/DayListItem';
 
 function DayList(props) {
-	return <ul></ul>;
+	const days = props.days;
+	const listDays = days.map((day) => (
+		<DayListItem
+			key={day.id}
+			//name={day.name}
+			//spots={day.spots}
+			{...day}
+			setDay={props.setDay}
+		/>
+	));
+	return <ul>{listDays}</ul>;
 }
 
 export default DayList;
