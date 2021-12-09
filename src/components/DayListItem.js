4537aff857f7ca82f03911282015b1ad22b04f2e
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-09 14:56:42 -0600

    Add relevent styles to each DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import 'components/DayListItem.scss';
+
+import classNames from 'classnames';
+
 export default function DayListItem(props) {
+	const { name, spots, selected, setDay } = props;
+	let dayClass = classNames('day-list__item', {
+		' day-list__item--selected': selected === true,
+		' day-list__item--full': spots === 0,
+	});
+
 	return (
-		<li>
-			<h2 className='text--regular'>Day Name</h2>
-			<h3 className='text--light'>X spots remaining</h3>
+		<li className={dayClass} onClick={() => setDay(name)}>
+			<h2 className='text--regular'>{name}</h2>
+			<h3 className='text--light'>{spots} spots remaining</h3>
 		</li>
 	);
 }
