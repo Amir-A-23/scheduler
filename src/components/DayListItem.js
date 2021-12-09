06377d77581ff3d59d0b81e939707fdc9cd24d4b
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-09 15:21:55 -0600

    Add function to format text to display relevant remaining spot information

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,8 +4,21 @@ import 'components/DayListItem.scss';
 
 import classNames from 'classnames';
 
+function formatSpots(spots) {
+	if (spots === 0) {
+		return 'no spots remaining';
+	}
+	if (spots === 1) {
+		return `${spots} spot remaining`;
+	}
+	if (spots > 1) {
+		return `${spots} spots remaining`;
+	}
+}
+
 export default function DayListItem(props) {
 	const { name, spots, selected, setDay } = props;
+
 	let dayClass = classNames('day-list__item', {
 		' day-list__item--selected': selected === true,
 		' day-list__item--full': spots === 0,
@@ -14,7 +27,7 @@ export default function DayListItem(props) {
 	return (
 		<li className={dayClass} onClick={() => setDay(name)}>
 			<h2 className='text--regular'>{name}</h2>
-			<h3 className='text--light'>{spots} spots remaining</h3>
+			<h3 className='text--light'>{formatSpots(spots)}</h3>
 		</li>
 	);
 }
