a6b7931cfd73b42bea7d3aaa40c9dd4cc2e49900
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-09 19:16:09 -0600

    Refactor InterviewerListItem component to include props, onClick event handler, conditional selection and conditional rendering

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 import 'components/InterviewerListItem.scss';
+import classNames from 'classnames';
 
 function InterviewerListItem(props) {
-	return (
-		<li className='interviewers__item'>
-			<img
-				className='interviewers__item-image'
-				src='https://i.imgur.com/LpaY82x.png'
-				alt='Sylvia Palmer'
-			/>
-			Sylvia Palmer
+	const { id, name, avatar, selected, setInterviewer } = props;
+
+	let interviewerClass = classNames('interviewers__item', {
+		'interviewers__item--selected': selected,
+	});
+
+	return selected ? (
+		<li className={interviewerClass} onClick={() => setInterviewer(id)}>
+			<img className='interviewers__item-image' src={avatar} alt={name} />
+			{name}
+		</li>
+	) : (
+		<li className={interviewerClass} onClick={() => setInterviewer(id)}>
+			<img className='interviewers__item-image' src={avatar} alt={name} />
 		</li>
 	);
 }
