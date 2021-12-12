9082162aa8e9aa6d6ba3bc5604033a7a55b113a9
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-12 12:13:13 -0600

    Add useEffect hook to make a request to the api to store and display the data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,11 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'components/Application.scss';
 
 import DayList from 'components/DayList';
 
 import 'components/Appointment';
 import Appointment from 'components/Appointment';
-const days = [
-	{
-		id: 1,
-		name: 'Monday',
-		spots: 2,
-	},
-	{
-		id: 2,
-		name: 'Tuesday',
-		spots: 5,
-	},
-	{
-		id: 3,
-		name: 'Wednesday',
-		spots: 0,
-	},
-];
 
 const appointmentsArr = [
 	{
@@ -65,6 +48,22 @@ const appointmentsArr = [
 
 export default function Application(props) {
 	const [day, setDay] = useState('Monday');
+	const [days, setDays] = useState([]);
+
+	useEffect(() => {
+		const dayURL = '/api/days';
+		axios
+			.get(dayURL)
+			.then((response) => {
+				console.log(response.data);
+				setDays([...response.data]);
+			})
+			.catch((error) => {
+				console.log(error.response.status);
+				console.log(error.response.headers);
+				console.log(error.response.data);
+			});
+	}, []);
 
 	const appointments = appointmentsArr.map((appointment) => {
 		return <Appointment key={appointment.id} {...appointment} />;
