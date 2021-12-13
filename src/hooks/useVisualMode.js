a5604edee7fbe91ae3286e6405664d54a712bdab
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-12 19:26:10 -0600

    Add complete custom useVisualMode hook with passing tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,26 @@
 import { useState } from 'react';
 
 function useVisualMode(initial) {
-	const [mode, setmode] = useState(initial);
-	return { mode };
+	const [mode, setMode] = useState(initial);
+	const [history, setHistory] = useState([initial]);
+	function transition(newMode, replace = false) {
+		if (replace) {
+			setMode(newMode);
+		} else {
+			setMode(newMode);
+			setHistory([...history, newMode]);
+		}
+	}
+	function back() {
+		if (history.length === 1) {
+			return;
+		}
+		setMode(history[history.length - 2]);
+		//	console.log('HISTORY', typeof history);
+		setHistory(history.slice(0, -1));
+	}
+
+	return { mode, transition, back };
 }
 
 export default useVisualMode;
