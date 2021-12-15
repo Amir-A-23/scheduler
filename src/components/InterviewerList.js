5e9bcf03e1a8c06e30f13752ea4c9ca2f7c100e3
Amir-A-23 <amir.a.23@hotmail.com>
2021-12-15 17:39:07 -0600

    Add Appointment test file

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'components/InterviewerList.scss';
 import InterviewerListItem from 'components/InterviewerListItem';
+import PropTypes from 'prop-types';
 
 function InterviewerList(props) {
 	const listOfInterviewers = props.interviewers.map((interviewer) => {
@@ -22,4 +23,7 @@ function InterviewerList(props) {
 	);
 }
 
+InterviewerList.propTypes = {
+	interviewers: PropTypes.array.isRequired,
+};
 export default InterviewerList;
